e7adf9b2fc979749bfc30c0f6a3514a80d560499
Dominik <d.beczyczko@gmail.com>
2019-10-25 18:51:12 +0200

    move navigation code to nav component

--- BeerBasement/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ BeerBasement/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { MatSidenav } from "@angular/material";
+import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+    selector: 'app-nav-menu',
+    templateUrl: './nav-menu.component.html',
+    styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnDestroy, OnInit {
+    private readonly mobileMatchQuery = '(max-width: 600px)';
 
-  collapse() {
-    this.isExpanded = false;
-  }
+    mobileQuery: MediaQueryList;
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
+    @ViewChild(MatSidenav, { static: true }) sideNav: MatSidenav;
+
+    private mobileQueryListener: (any) => void;
+
+    constructor(changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia(this.mobileMatchQuery);
+        this.mobileQueryListener = (event) => {
+            changeDetectorRef.detectChanges();
+            this.sideNav.toggle(!event.matches);
+        };
+        this.mobileQuery.addListener(this.mobileQueryListener);
+    }
+
+    ngOnInit(): void {
+        const isMobile = this.media.matchMedia(this.mobileMatchQuery).matches;
+        this.sideNav.toggle(!isMobile);
+    }
+
+    ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this.mobileQueryListener);
+    }
 }
